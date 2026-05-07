a9976fa83c858ea076f1910d5b349a5f08d68999
Bryan Starbuck <Bryan@thestarbucks.com>
2026-05-07 11:22:35 -0700

    Bryan 19

--- cloudfront-function.js
+++ cloudfront-function.js
@@ -1,3 +1,13 @@
+// Pages deleted from this site that Google still has indexed.
+// 301 to a new home where one exists; 410 Gone tells Google to drop the URL fast.
+var REDIRECTS_301 = {
+    '/uaps/Details/Frank_Olson/': 'https://intelligencemurders.com/intelligence-service-murders/Details/Frank_Olson/'
+};
+var GONE_410 = {
+    '/uaps/Details/William_Cooper/': true,
+    '/physics/Details/William_Cooper/': true
+};
+
 function handler(event) {
     var request = event.request;
     var host = request.headers.host && request.headers.host.value;
@@ -19,16 +29,39 @@ function handler(event) {
 
     var lastSeg = uri.substring(uri.lastIndexOf('/') + 1);
     var needsTrailingSlash = uri !== '/' && uri.charAt(uri.length - 1) !== '/' && lastSeg.indexOf('.') === -1;
+    var canonicalUri = needsTrailingSlash ? uri + '/' : uri;
 
     if (host === 'www.uapmurders.com') {
-        var target = uri + (needsTrailingSlash ? '/' : '');
         return {
             statusCode: 301,
             statusDescription: 'Moved Permanently',
             headers: {
-                'location': { value: 'https://uapmurders.com' + target + qs },
+                'location': { value: 'https://uapmurders.com' + canonicalUri + qs },
                 'cache-control': { value: 'max-age=3600' }
             }
+        };
+    }
+
+    if (REDIRECTS_301[canonicalUri]) {
+        return {
+            statusCode: 301,
+            statusDescription: 'Moved Permanently',
+            headers: {
+                'location': { value: REDIRECTS_301[canonicalUri] },
+                'cache-control': { value: 'max-age=86400' }
+            }
+        };
+    }
+
+    if (GONE_410[canonicalUri]) {
+        return {
+            statusCode: 410,
+            statusDescription: 'Gone',
+            headers: {
+                'content-type': { value: 'text/html; charset=utf-8' },
+                'cache-control': { value: 'max-age=86400' }
+            },
+            body: '<!doctype html><title>410 Gone</title><h1>Gone</h1><p>This page has been permanently removed.</p>'
         };
     }
 
